1c056a85dce8a534ad7ca59bd233736277a10bb2
vvchn <vl.soldatov4@gmail.com>
2025-03-13 21:35:33 +0300

    Declare dictionaries controller skeleton

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,8 +1,10 @@
 package com.example
 
+import com.example.controllers.DictionariesController
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.routing.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
@@ -10,5 +12,11 @@ fun main() {
 }
 
 fun Application.module() {
-    configureRouting()
+    val controller = DictionariesController(null)
+
+    routing {
+        with(controller) {
+            configureRouting()
+        }
+    }
 }
