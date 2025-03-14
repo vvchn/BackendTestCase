8a64f95d82dc491671534dc973bc8117c9699c8d
vvchn <vl.soldatov4@gmail.com>
2025-03-14 22:23:41 +0300

    Make a max name length a constant

--- src/main/kotlin/data/Tables.kt
+++ src/main/kotlin/data/Tables.kt
@@ -5,14 +5,16 @@ import kotlinx.serialization.json.Json
 import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.json.json
 
+const val MAX_NAME_LENGTH = 50
+
 object Dictionaries : Table() {
-    val name = varchar("name", 50).uniqueIndex()
+    val name = varchar("name", MAX_NAME_LENGTH).uniqueIndex()
     val structure = json<DictionaryStructure>("structure", Json { ignoreUnknownKeys = true })
 }
 
 object DictionaryRecords : Table() {
     val id = integer("id").autoIncrement()
-    val dictionaryName = varchar("dictionary_name", 50).references(Dictionaries.name)
+    val dictionaryName = varchar("dictionary_name", MAX_NAME_LENGTH).references(Dictionaries.name)
     val data = text("data")
     override val primaryKey = PrimaryKey(id)
 }
