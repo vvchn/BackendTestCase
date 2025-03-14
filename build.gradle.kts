6ebe09f0a00300cf40bf88b3cc36e5e020d05c42
vvchn <vl.soldatov4@gmail.com>
2025-03-14 17:26:16 +0300

    Add Detekt code analyzer

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,24 @@
+import io.gitlab.arturbosch.detekt.Detekt
 
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.ktor)
+    alias(libs.plugins.detekt)
 }
+
+private val javaVersion = "17"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(javaVersion))
+    }
+}
+
+// Скачает ли оно само жаву, если таковой не будет?
+private val javaToolchainService = project.extensions.getByType<JavaToolchainService>()
+private val jdkPath = javaToolchainService.launcherFor {
+    languageVersion.set(JavaLanguageVersion.of(javaVersion.toInt()))
+}.get().metadata.installationPath.asFile.absolutePath
 
 group = "com.example"
 version = "0.0.1"
@@ -26,6 +42,28 @@ tasks.withType<JavaExec>().configureEach {
     }
 }
 
+detekt {
+    toolVersion = libs.versions.detekt.get()
+    allRules = true
+    ignoreFailures = true
+}
+
+tasks.withType<Detekt>().configureEach {
+    this.jvmTarget = javaVersion
+    jdkHome.set(file(jdkPath))
+
+    reports {
+        html {
+            required.set(true)
+            outputLocation.set(file("${project.rootDir}/detekt/reports/detektReport.html"))
+        }
+        xml.required.set(false)
+        md.required.set(false)
+        txt.required.set(false)
+        sarif.required.set(false)
+    }
+}
+
 dependencies {
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.netty)
@@ -33,4 +71,6 @@ dependencies {
     implementation(libs.ktor.server.core)
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
+
+    detektPlugins(libs.detekt.formatting)
 }
